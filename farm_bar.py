8333c1ef3b70ef03ff225630addfa106a7e2662f
SaRai <74460961+aikidoaikido115@users.noreply.github.com>
2022-07-09 20:46:20 +0700

    Add files via upload

---
+++ farm_bar.py
@@ -0,0 +1,5 @@
+from template import bar
+
+d = [50,40,70]
+
+bar('อะไรก็ได้1','อะไรก็ได้2','อะไรก็ได้3',title='อะไรก็ได้',percent=d)
--- template.py
+++ template.py
@@ -11,18 +11,17 @@ def bar(*args,title,percent):
     plt.ylabel('เปอร์เซ็นต์ (%)',color='orange',fontsize=25)
 
     x = []
-    y = percent #ต้องแก้เอง
+    y = percent
     for i in args:
         x.append(i)
 
 
     plt.bar(x,y,width=0.42,color='#6de8c3')
 
-    plt.savefig(r'.\new.jpg')
-    plt.show()
+    plt.savefig(r'.\bar.jpg')
+    # plt.show()
     
 
-
-
 if __name__ == '__main__':
+    
     bar('กรุงเทพ','ปริมณฑล','อื่นๆ','เชียงใหม่',title='user',percent=[100,50,4,20])
